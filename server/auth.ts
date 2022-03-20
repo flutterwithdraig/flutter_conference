f5236b603489ac2998c1de562153a1397d2d1579
Draig <gameofdraig@gmail.com>
2022-03-20 13:40:27 +0000

    Episode 5

--- server/auth.ts
+++ server/auth.ts
@@ -1,6 +1,8 @@
 import { NextFunction, Request, Response } from 'express';
 import { admin } from './firestore';
 
+const adminKey = 'letmein'
+
 const authMiddleWare = async (req: Request, res: Response, next: NextFunction) => {
 
     try {
@@ -9,11 +11,34 @@ const authMiddleWare = async (req: Request, res: Response, next: NextFunction) =
     req.uid = decodedToken.uid
     } catch (e) {
         req.uid = null
-        return res.status(401).send('Unauthorized')
     }
 
-
+    req.admin = req.query.key === adminKey || req.body.key === adminKey
     next()
 }
 
-export {authMiddleWare}
+const requireAuth = (req: Request, res: Response, next: NextFunction) => {
+    if(req.uid || req.admin) {
+        next()
+    } else {
+        return res.status(401).send('Unauthorised')
+    }
+}
+
+const requireAdmin = (req: Request, res: Response, next: NextFunction) => {
+    if(req.admin) {
+        next()
+    } else {
+        return res.status(401).send('Unauthorised - admin required')
+    }
+}
+
+const requireSelf = (req: Request, res: Response, next: NextFunction) => { 
+    if(req.uid === req.params.id || req.admin){
+        next()
+    } else {
+        return res.status(401).send('Unauthorised - You can only edit yourself')
+    }
+}
+
+export {authMiddleWare, requireAuth, requireAdmin, requireSelf}
---
+++ server/controllers/users.ts
@@ -0,0 +1,69 @@
+import { Router } from 'express';
+import { requireAdmin, requireSelf } from '../auth';
+import { admin, db } from '../firestore';
+
+
+const router = Router()
+
+// C R U D 
+
+router.post('/', requireAdmin, async (req, res) => {
+
+    const email = req.body.email
+    const password = req.body.password
+    const displayName = req.body.display_name
+    const profile = req.body.profile
+
+    if(!email || !password || !displayName || !profile){
+        return res.status(422).json({err: 'Missing data'})
+    }
+
+    // Setup user with firebase auth
+    const user = await admin.auth().createUser({
+       email,
+       password,
+       displayName, 
+    })
+
+    // Store user details in the firestore users collection
+    await db.collection('users').doc(user.uid).create({
+        name: displayName,
+        profile,
+        created: admin.firestore.FieldValue.serverTimestamp(),
+        updated: admin.firestore.FieldValue.serverTimestamp(),
+    })
+
+    res.json({msg: 'User created', uid: user.uid})
+})
+
+
+router.get('/', async (req, res) => {
+
+    console.log(req.uid)
+
+    const documentData = await db.collection('users').get()
+    const result = documentData.docs.map((d) => ({
+        id: d.id,
+        ...d.data()
+    }))
+    res.json(result);
+})
+
+router.patch('/:id', requireSelf, async (req, res) => {
+    res.send('OK')
+})
+
+router.get('/:id', async (req, res) => {
+    const documentSnap = await db.collection('users').doc(req.params.id).get()
+
+    if(!documentSnap.exists){
+        return res.status(404).json({error: 'User not found'})
+    }
+
+    res.json({
+        id: documentSnap.id,
+        ...documentSnap.data()
+    })
+})
+
+export { router as UsersController}
